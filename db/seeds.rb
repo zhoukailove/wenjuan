df96045d9138b1dbde29d8f6cd10064bfdff761e
周凯 <fayake@163.com>
2016-11-19 17:32:13 +0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
 User.create!(name:  "周凯",
              login_name: "zk",
              password:              "111"
